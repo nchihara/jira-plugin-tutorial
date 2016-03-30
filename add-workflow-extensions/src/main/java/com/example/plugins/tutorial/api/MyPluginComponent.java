41cdb29ec28c833ef4dada3c29f1a9d283ce0e5a
N.Chihara <nchihara@go2group.com>
2016-03-30 18:13:58 +0900

    (on-going) Creating workflow extensions

---
+++ add-workflow-extensions/src/main/java/com/example/plugins/tutorial/api/MyPluginComponent.java
@@ -0,0 +1,6 @@
+package com.example.plugins.tutorial.api;
+
+public interface MyPluginComponent
+{
+    String getName();
+}
---
+++ add-workflow-extensions/src/main/java/com/example/plugins/tutorial/jira/workflow/ParentIssueBlockingCondition.java
@@ -0,0 +1,53 @@
+package com.example.plugins.tutorial.jira.workflow;
+
+import com.atlassian.jira.component.ComponentAccessor;
+import com.atlassian.jira.issue.Issue;
+import com.atlassian.jira.workflow.WorkflowFunctionUtils;
+import com.atlassian.jira.workflow.condition.AbstractJiraCondition;
+import com.opensymphony.module.propertyset.PropertySet;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class ParentIssueBlockingCondition extends AbstractJiraCondition
+{
+    private static final Logger log = LoggerFactory.getLogger(ParentIssueBlockingCondition.class);
+
+    public static final String FIELD_WORD = "word";
+
+    public boolean passesCondition(Map transientVars, Map args, PropertySet ps)
+    {
+        Issue subTask = (Issue) transientVars.get(WorkflowFunctionUtils.ORIGINAL_ISSUE_KEY);
+
+        Issue parentIssue = ComponentAccessor.getIssueManager().getIssueObject(subTask.getParentId());
+        if (parentIssue == null)
+        {
+            return false;
+        }
+
+        String statuses = (String) args.get("statuses");
+        StringTokenizer st = new StringTokenizer(statuses, ",");
+
+        while(st.hasMoreTokens())
+        {
+            String statusId = st.nextToken();
+            if (parentIssue.getStatus().getId().equals(statusId))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+/***
+    public boolean passesCondition(Map transientVars, Map args, PropertySet ps)
+    {
+        String word = (String)transientVars.get(FIELD_WORD);
+        Issue issue = getIssue(transientVars);
+        String description = issue.getDescription();
+
+        return description != null && description.contains(word);
+    }
+***/
+}
